1f4e443be42e017aa37af67f51c96c9637ffc1e0
Fabio Jun <fabiojundev@gmail.com>
2022-07-04 17:37:53 -0300

    Change contract/script to mint image

--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,15 @@ const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory("MyEpicNFT");
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
-    console.log("Contrato implantado em:", nftContract.address);
+    console.log("Contrato implantado em:", nftContract.address)
+    // Chama a função.
+    let txn = await nftContract.makeAnEpicNFT();
+    // Espera ela ser minerada.
+    await txn.wait();
+    // Minta outra NFT por diversão.
+    txn = await nftContract.makeAnEpicNFT();
+    // Espera ela ser minerada.
+    await txn.wait();
 };
 const runMain = async () => {
     try {
