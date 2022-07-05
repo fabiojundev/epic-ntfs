9f667e091476c93b92c2a936db279c0cb272a561
Fabio Jun <fabiojundev@gmail.com>
2022-07-05 20:21:28 -0300

    Add random background colors

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,26 +1,23 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.1;
 
-// Precisamos de algumas funcoes utilitarias.
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-// Precisamos importar essa funcao de base64 que acabamos de criar
 import {Base64} from "./libraries/Base64.sol";
 
 contract MyEpicNFT is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    // Aqui esta o codigo do nosso SVG. So precisaremos alterar as palavras que vao ser exibidas. Todo o resto permanece igual.
-    // Entao, fazemos uma variavel baseSvg aqui que todos os nossos NFTs vao usar.
-    string baseSvg =
-        "<svg  xmlns='http://www.w3.org/2000/svg'  preserveAspectRatio='xMinYMin meet'  viewBox='0 0 350 350'>  <defs>    <linearGradient id='Gradient1'>      <stop class='stop1' offset='0%'/>      <stop class='stop2' offset='50%'/>      <stop class='stop3' offset='100%'/>    </linearGradient>  </defs>  <style>    .base {      fill: blue;      font-family: serif;      font-size: 20px;      color: #FFF;    }    .stop1 { stop-color: green; }    .stop2 { stop-color: white; stop-opacity: 0; }    .stop3 { stop-color: yellow; }      </style>  <rect width='100%' height='100%' fill='url(#Gradient1)' />  <text    x='50%'    y='50%'    class='base'    dominant-baseline='middle'    text-anchor='middle'  >";
+    string svgPartOne =
+        "<svg  xmlns='http://www.w3.org/2000/svg'  preserveAspectRatio='xMinYMin meet'  viewBox='0 0 350 350'>  <defs>    <linearGradient id='Gradient1'>      <stop class='stop1' offset='0%'/>      <stop class='stop2' offset='50%'/>      <stop class='stop3' offset='100%'/>    </linearGradient>  </defs>  <style>    .base {      fill: blue;      font-family: serif;      font-size: 20px;      color: #FFF;    }    .stop1 { stop-color: ";
 
-    // Eu crio tres listas, cada uma com seu grupo de palavras aleatorias
-    // escolha as suas palavras divertidas, nome de personagem, comida, time de futebol, o que quiser!
+    string svgPartTwo =
+        "; }    .stop2 { stop-color: white; stop-opacity: 0; }    .stop3 { stop-color: yellow; }      </style>  <rect width='100%' height='100%' fill='url(#Gradient1)' />  <text    x='50%'    y='50%'    class='base'    dominant-baseline='middle'    text-anchor='middle'  >";
+
     string[] firstWords = [
         "Natal",
         "Navegantes",
@@ -63,25 +60,25 @@ contract MyEpicNFT is ERC721URIStorage {
         "Torrada"
     ];
 
+    // Cores divertidas! Declarando um monte de cores
+    string[] colors = ["red", "#08C2A8", "black", "yellow", "blue", "green"];
+
     event NewEpicNFTMinted(address sender, uint256 tokenId);
 
     constructor() ERC721("ChavesNFT", "CHAVO") {
         console.log("Meu contrato de NFT! Tchu-hu");
     }
 
-    // Crio uma funcao que pega uma palavra aleatoria da lista.
     function pickRandomFirstWord(uint256 tokenId)
         public
         view
         returns (string memory)
     {
-        // Crio a 'semente' para o gerador aleatorio. Mais sobre isso na licao.
         uint256 rand = random(
             string(
                 abi.encodePacked("PRIMEIRA_PALAVRA", Strings.toString(tokenId))
             )
         );
-        // pego o numero no maximo ate o tamanho da lista, para nao dar erro de indice.
         rand = rand % firstWords.length;
         return firstWords[rand];
     }
@@ -114,6 +111,19 @@ contract MyEpicNFT is ERC721URIStorage {
         return thirdWords[rand];
     }
 
+    // Mesma coisa de sempre, pega uma cor aleatória.
+    function pickRandomColor(uint256 tokenId)
+        public
+        view
+        returns (string memory)
+    {
+        uint256 rand = random(
+            string(abi.encodePacked("COLOR", Strings.toString(tokenId)))
+        );
+        rand = rand % colors.length;
+        return colors[rand];
+    }
+
     function random(string memory input) internal pure returns (uint256) {
         return uint256(keccak256(abi.encodePacked(input)));
     }
@@ -129,9 +139,17 @@ contract MyEpicNFT is ERC721URIStorage {
             abi.encodePacked(first, second, third)
         );
 
+        string memory randomColor = pickRandomColor(newItemId);
+
         // Concateno tudo junto e fecho as tags <text> e <svg>.
         string memory finalSvg = string(
-            abi.encodePacked(baseSvg, combinedWord, "</text></svg>")
+            abi.encodePacked(
+                svgPartOne,
+                randomColor,
+                svgPartTwo,
+                combinedWord,
+                "</text></svg>"
+            )
         );
 
         // pego todos os metadados de JSON e codifico com base64.
@@ -157,15 +175,7 @@ contract MyEpicNFT is ERC721URIStorage {
         );
 
         console.log("\n--------------------");
-        // console.log(finalTokenUri);
-        console.log(
-            string(
-                abi.encodePacked(
-                    "https://nftpreview.0xdev.codes/?code=",
-                    finalTokenUri
-                )
-            )
-        );
+        console.log(finalTokenUri);
         console.log("--------------------\n");
 
         _safeMint(msg.sender, newItemId);
@@ -179,7 +189,6 @@ contract MyEpicNFT is ERC721URIStorage {
             newItemId,
             msg.sender
         );
-
         emit NewEpicNFTMinted(msg.sender, newItemId);
     }
 }
